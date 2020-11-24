8793a1a089c21d0a9ece96548acda1f934973514
Daniel Olarte <daniel@voiq.com>
2020-11-24 12:31:43 -0500

    Updated title and favicon

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router } from "react-router-dom";
+import {Helmet} from "react-helmet";
 
 import Container from "./components/Container";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>VOIQ | Browser Bot</title>
+    </Helmet>
     <Router>
       <Container />
     </Router>
